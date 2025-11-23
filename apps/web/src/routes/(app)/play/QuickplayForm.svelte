fa0d37f59b9747a5a07f23684f8b249c5c130e21
Johnny Magrippis <j@magrippis.com>
2025-11-23 13:43:34 +0100

    ✨ icons and loading state for the "Quickplay" or "Join" room play options

--- apps/web/src/routes/(app)/play/QuickplayForm.svelte
+++ apps/web/src/routes/(app)/play/QuickplayForm.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
+	import QuickplayIcon from '~icons/mdi/world'
 	import {Button} from '$lib/components/ui/button'
+	import Spinner from '$lib/components/ui/spinner/spinner.svelte'
 	import {quickplay} from './join.remote'
 </script>
 
 <form {...quickplay}>
-	<Button class="w-full" size="lg">Quick Play</Button>
+	<Button class="w-full" size="lg">
+		{#if quickplay.pending}
+			<Spinner />
+		{:else}
+			<QuickplayIcon />
+		{/if}
+		Quickplay
+	</Button>
 </form>
--- apps/web/src/routes/(app)/play/RoomCodeForm.svelte
+++ apps/web/src/routes/(app)/play/RoomCodeForm.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import JoinRoomIcon from '~icons/system-uicons/enter'
 	import {Button} from '$lib/components/ui/button'
 	import * as Field from '$lib/components/ui/field'
 	import * as InputOTP from '$lib/components/ui/input-otp'
+	import Spinner from '$lib/components/ui/spinner/spinner.svelte'
 	import {joinRoom} from './join.remote'
 
 	const PLAYING_CARDS_REGEX_STRING = '^[2-9jJqQkKaA]+$'
@@ -29,7 +31,14 @@
 					</InputOTP.Group>
 				{/snippet}
 			</InputOTP.Root>
-			<Button class="grow">Join!</Button>
+			<Button class="grow">
+				{#if joinRoom.pending}
+					<Spinner />
+				{:else}
+					<JoinRoomIcon />
+				{/if}
+				Join!
+			</Button>
 		</div>
 
 		<Field.Description>Enter a room code to join!</Field.Description>
